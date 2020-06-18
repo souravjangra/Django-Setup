270c019349c10aa04724055f5b4f7d4489b2ba30
Sourav Jangra <imsouravjangra@Souravs-MacBook-Air.local>
2020-06-18 19:12:52 +0530

    working with django-forms

--- django-setup/Django_Setup/products/views.py
+++ django-setup/Django_Setup/products/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
