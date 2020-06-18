685946f5fe11c22f61ce38c250bb8f90a020bba8
Sourav Jangra <imsouravjangra@Souravs-MacBook-Air.local>
2020-06-18 17:09:49 +0530

    rendering different views and html using django builtin template tags and filters

--- django-setup/Django_Setup/pages/views.py
+++ django-setup/Django_Setup/pages/views.py
@@ -12,4 +12,10 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "title": "we are in about page",
+        "my_number": 123,
+        "my_list": [12,21,34,45,52, "Abc"],
+        "my_title": '<h1>Hello World</h1>'
+    }
+    return render(request, "about.html", my_context)
