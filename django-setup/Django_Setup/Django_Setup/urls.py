edaafcf4da448ddf11be953b3e6604357555e3aa
Sourav Jangra <imsouravjangra@Souravs-MacBook-Air.local>
2020-06-18 17:46:48 +0530

    rendering data from the database

--- django-setup/Django_Setup/Django_Setup/urls.py
+++ django-setup/Django_Setup/Django_Setup/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('contact', contact_view),
-    path('about', about_view),
+    path('contact/', contact_view),
+    path('about/', about_view),
+    path('product/', product_detail_view),
     path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
 ]
--- django-setup/Django_Setup/products/views.py
+++ django-setup/Django_Setup/products/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'object': obj
+    }
+    return render(request, "products/product_detail.html", context)
